608088959505f2604c58b469343caaf8c99a026d
Infinity2010 <arbiter2008@ya.ru>
2019-05-20 18:31:28 +0300

    Добавлены методы, выполняющие синтаксический анализ значений, получаемых из формы

--- src/main/java/org/javaee7/movieplex7/booking/Booking.java
+++ src/main/java/org/javaee7/movieplex7/booking/Booking.java
@@ -5,10 +5,59 @@
  */
 package org.javaee7.movieplex7.booking;
 
+import java.util.StringTokenizer;
+import javax.faces.flow.FlowScoped;
+import javax.inject.Named;
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.PersistenceContext;
+import org.javaee7.movieplex7.entities.Movie;
+
 /**
  *
  * @author arbit
  */
+@Named
+@FlowScoped("booking")
 public class Booking {
+    @PersistenceContext
+    EntityManager em;
+    
+    private int movieId;
+    private String startTime;
+    private int startTimeId;
+    
+    public int getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(int movieId) {
+        this.movieId = movieId;
+    }
+
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(String startTime) {
+        StringTokenizer tokens = new StringTokenizer(startTime, ",");
+        startTimeId = Integer.parseInt(tokens.nextToken());
+        this.startTime = tokens.nextToken();
+    }
+
+    public int getStartTimeId() {
+        return startTimeId;
+    }
+    
+    public String getMovieName() {
+        try {
+            return em.createNamedQuery("Movie.findById", Movie.class)
+                    .setParameter("id", movieId)
+                    .getSingleResult()
+                    .getName();
+        } catch (NoResultException e) {
+            return "";
+        }
+    }
     
 }
