4eb1690f9cb0982d248720fc144f977da71e8300
Eternity074 <arbiter2008@ya.ru>
2019-05-23 12:25:54 +0300

    код для получения информации о конкретном фильме

--- src/main/java/org/javaee7/movieplex7/client/MovieClientBean.java
+++ src/main/java/org/javaee7/movieplex7/client/MovieClientBean.java
@@ -8,6 +8,7 @@ package org.javaee7.movieplex7.client;
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
@@ -25,6 +26,9 @@ public class MovieClientBean {
     Client client;
     WebTarget target;
     
+    @Inject
+    MovieBackingBean bean;
+    
     @PostConstruct
     public void init() {
         client = ClientBuilder.newClient();
@@ -38,6 +42,11 @@ public class MovieClientBean {
     
     public Movie[] getMovies() {
         return target.request().get(Movie[].class);
+    }
+    
+    public Movie getMovie() {
+        Movie m = target.path("{movie}").resolveTemplate("movie", bean.getMovieId()).request().get(Movie.class);
+        return m;
     }
     
 }
